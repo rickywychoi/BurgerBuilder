530e53cf5a3cf66d750d5d0b2b3e208270281ece
rickywychoi <rickywychoi@github.com>
2019-02-18 23:25:39 -0800

    spinner with orange background

---
+++ src/components/UI/Spinner/OrangeSpinner/OrangeSpinner.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import classes from './OrangeSpinner.css';
+
+const orangeSpinner = () => <div className={classes.Loader}>Loading...</div>
+
+export default orangeSpinner;
